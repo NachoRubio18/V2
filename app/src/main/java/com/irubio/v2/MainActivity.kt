f068ea0ed765c07a29627371b56810a8fe8bb09c
NachoRubio18 <i.rubio@edu.uah.es>
2023-02-26 12:50:15 +0100

    Tts funciona

--- app/src/main/java/com/irubio/v2/MainActivity.kt
+++ app/src/main/java/com/irubio/v2/MainActivity.kt
@@ -3,14 +3,16 @@ package com.irubio.v2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
-    private lateinit var tts: TextToSpeech
+    private var tts: TextToSpeech? = null
+    private var listo: Boolean? = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +26,37 @@ class MainActivity : AppCompatActivity() {
         val imageView = findViewById<ImageView>(R.id.imageView)
         val textView = findViewById<TextView>(R.id.textView)
 
+        tts = TextToSpeech(this, this)
+
 
         imageView.setOnClickListener{
             Toast.makeText(this, textView.text, Toast.LENGTH_SHORT).show()
+            val texto = textView!!.text.toString()
+            tts!!.speak(texto, TextToSpeech.QUEUE_FLUSH, null, "")
         }
 
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale("spa"))
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language not supported!")
+            } else {
+                listo = true
+            }
+        }
+    }
+
+    public override fun onDestroy() {
+
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        super.onDestroy()
     }
 
 
