96949ac293540a9b7e1c8ae6efc2f95c1cce5e66
oliver <oliveraltfer@gmail.com>
2023-03-18 21:02:09 +0100

    asdx

--- app/src/main/java/com/irubio/v2/MainActivity.kt
+++ app/src/main/java/com/irubio/v2/MainActivity.kt
@@ -1,64 +1,68 @@
 package com.irubio.v2
 
-import android.app.ActivityOptions
-import androidx.appcompat.app.AppCompatActivity
+import MyAdapter
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
-class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+class MainActivity : AppCompatActivity() {
 
     private var tts: TextToSpeech? = null
     private var listo: Boolean? = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        hola()
+        setContentView(R.layout.activity_recycle)
 
+        val data = listOf(
+            R.drawable.gato,
+            R.drawable.gato,
+            R.drawable.gato,
+            R.drawable.gato,
+            R.drawable.gato,
+            R.drawable.gato,
+            R.drawable.gato,
+            R.drawable.gato,
+        )
+
+        val recyclerView: RecyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        val adapter = MyAdapter(data)
+        val llm = LinearLayoutManager(this)
+        llm.setOrientation(LinearLayoutManager.VERTICAL)
+        recyclerView.setLayoutManager(llm)
+        recyclerView.setAdapter(adapter)
+            adapter.notifyDataSetChanged()
     }
 
 
-    private fun hola(){
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        val textView = findViewById<TextView>(R.id.textView)
-
-        tts = TextToSpeech(this, this)
 
 
-        imageView.setOnClickListener{
-            Toast.makeText(this, textView.text, Toast.LENGTH_SHORT).show()
-            val texto = textView!!.text.toString()
-            tts!!.speak(texto, TextToSpeech.QUEUE_FLUSH, null, "")
-        }
+    //override fun onInit(status: Int) {
+        //if (status == TextToSpeech.SUCCESS) {
+            //val result = tts!!.setLanguage(Locale("spa"))
 
-    }
+            //if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                //Log.e("TTS","The Language not supported!")
+            //} else {
+               // listo = true
+            //}
+        //}
+    //}
 
-    override fun onInit(status: Int) {
-        if (status == TextToSpeech.SUCCESS) {
-            val result = tts!!.setLanguage(Locale("spa"))
+    //public override fun onDestroy() {
 
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS","The Language not supported!")
-            } else {
-                listo = true
-            }
-        }
-    }
+        //if (tts != null) {
+            //tts!!.stop()
+            //tts!!.shutdown()
+        //}
 
-    public override fun onDestroy() {
-
-        if (tts != null) {
-            tts!!.stop()
-            tts!!.shutdown()
-        }
-
-        super.onDestroy()
-    }
+        //super.onDestroy()
+    //}
 
 
 }
